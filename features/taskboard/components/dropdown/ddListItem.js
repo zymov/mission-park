32b8d805c9acd948219736d2287b0a8583ff0791
zym <eyeder@163.com>
2017-05-01 13:06:01 +0800

    change user avatar in taskboard

--- features/taskboard/components/dropdown/ddListItem.js
+++ features/taskboard/components/dropdown/ddListItem.js
@@ -13,7 +13,7 @@ class DDListItem extends React.Component {
 	  return (
 	    <li>
 	    	<a className="dd-list-item" href="javascript:void(0);" title={item.name || item} onClick={itemClick.bind(this, item)}>
-	    		{ item.name && <img src="/static/imgs/100.png"/>}
+	    		{ item.name && <img src={`https://api.adorable.io/avatars/32/${item._id}@adorable.io.png`} title={item.name} />}
 	    		{ item.name || item }
 	    		{ selected && <i className="glyphicon glyphicon-ok"></i>}
 	    	</a>
--- features/taskboard/components/infoLabel.js
+++ features/taskboard/components/infoLabel.js
@@ -8,7 +8,13 @@ class InfoLabel extends React.Component {
 
 		return(
 			<li className="removable">
-				<a title={item.name || item}>{item.name && <img src="/static/imgs/100.png" />}{item.name || item}</a>
+				<a title={item.name || item}>
+					{item.name && 
+						<img 
+							src={`https://api.adorable.io/avatars/24/${item._id}@adorable.io.png`} 
+							title={item.name} />}
+					{item.name || item}
+				</a>
 				{removable && <span className="remove-label glyphicon glyphicon-remove" onClick={labelClick.bind(this, item)}></span>}
 			</li>
 		);
