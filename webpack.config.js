e053252fc8d00fa8d83acfb0edb18ce3bda938a2
zym <eyeder@163.com>
2017-07-22 13:04:28 +0800

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,51 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
+var path = require("path");
+var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
 	entry: [
-	  // 'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
-	  // 'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-	  './app/rootRouter.js' // Your appʼs entry point
+	  // "webpack-dev-server/client?http://0.0.0.0:3000", // WebpackDevServer host and port
+	  // "webpack/hot/only-dev-server", // "only" prevents reload on syntax errors
+	  "./app/rootRouter.js" // Your appʼs entry point
 	],
 	output: {
-		path: path.join(__dirname, 'dist'),
-		filename: 'app.bundle.js',
+		path: path.join(__dirname, "dist"),
+		filename: "app.bundle.js",
 		publicPath: __dirname
 	},
 	module: {
-	  loaders: [
+	  rules: [
 	    { 
 	    	test: /\.(js|jsx)?$/, 
 	    	exclude: /node_modules/, 
-	    	loaders: ['react-hot', 'babel-loader?presets[]=es2015&presets[]=react']
+	    	loader: "babel-loader",
+	    	options: {
+	    		"presets": [ "es2015", "react" ]
+	    	}
+	    },
+	    {
+	    	test: /\.css$/,
+	    	use: [
+	    		{
+	    			loader: "style-loader",
+	    		},
+	    		{
+	    			loader: "css-loader",
+	    			options: {
+	    				modules: true
+	    			}
+	    		}
+	    	]
+
 	    }
 	  ]
 	},
-	plugins: [
-	  new webpack.HotModuleReplacementPlugin()
-	]
+	// plugins: [
+	//   new UglifyJsPlugin({
+	//   	sourceMap: true,
+	//   	compress: {
+	//   		warnings: true
+	//   	}
+	//   })
+	// ]
 }
